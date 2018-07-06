45bfb04a60681e967ad633eda7443b615e3ff60d
LuoHao <396912848@qq.com>
2018-07-06 16:14:46 +0800

    set basic configration and UI

--- PomatoWidget/TodayViewController.swift
+++ PomatoWidget/TodayViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 import NotificationCenter
-
+import SnapKit
 class TodayViewController: UIViewController, NCWidgetProviding {
+    lazy var button: UIButton = {
         
+        let btn=UIButton(type: .custom);
+        btn.setTitle("进入到Pomato", for: .normal)
+    
+        self.view.addSubview(btn)
+        
+        btn.addTarget(self, action: #selector(click(btn:)), for: .touchUpInside)
+        return btn
+    }()
+    
+    @objc public func click(btn:UIButton) -> Void {
+        self.extensionContext?.open(URL(string: "pomato.mainapp://")!, completionHandler: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
+        //    self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+        self.button.snp.makeConstraints { (make) in
+            make.width.height.equalTo(self.view)
+            make.center.equalToSuperview()
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,6 +39,10 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Dispose of any resources that can be recreated.
     }
     
+    @available(iOSApplicationExtension 10.0, *)
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        self.preferredContentSize = (activeDisplayMode == .compact) ? maxSize : CGSize(width: maxSize.width, height: 200)
+    }
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
